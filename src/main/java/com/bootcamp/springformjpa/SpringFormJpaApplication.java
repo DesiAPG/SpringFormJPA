5a03c65c8077d957d5234fe8cf1211695072c1df
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-21 10:48:46 -0500

    First Commit

---
+++ src/main/java/com/bootcamp/springformjpa/SpringFormJpaApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.springformjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFormJpaApplication.class, args);
+    }
+
+}
